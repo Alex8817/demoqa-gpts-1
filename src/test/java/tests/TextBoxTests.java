0b29d5127108794cdb2818ccad8705034402add7
Alex8817 <alex.ivanov0988@gmail.com>
2025-02-16 11:49:31 +0200

    added fix modal

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,11 +1,15 @@
 package tests;
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class TextBoxTests extends TestBase {
 
@@ -13,6 +17,8 @@ public class TextBoxTests extends TestBase {
     void fillFormTest() {
         open("/text-box");
 
+        if($(".fc-dialog-content").isDisplayed())
+            $(byText("Consent")).click();
         $("#userName").setValue("Alex Egorov");
         $("#userEmail").setValue("alex@egorov.com");
         $("#currentAddress").setValue("Some address 1");
